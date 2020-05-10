6743ce0de5e5433086bbbb7c7758aa4350a29059
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-05-10 17:32:34 -0400

    Update COCI14Contest7#3ACM.cpp

--- COCI/COCI14Contest7#3ACM.cpp
+++ COCI/COCI14Contest7#3ACM.cpp
@@ -7,27 +7,10 @@ using namespace std;
 #define nl '\n'
 void sc(){}template<class T,class...A>void sc(T&t,A&...a){cin>>t,sc(a...);}
 void pr(){}template<class T,class...A>void pr(T t,A...a){cout<<t,pr(a...);}
-#define sz(x) (int)(x.size())
-#define af(x) x.begin(), x.end()
-#define sqr(x) (x * x)
-#define lsb(x) (x & -x)
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define lb lower_bound
-#define ub upper_bound
-#define f first
-#define s second
-const int mod = 1e9 + 7, base = 131;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
 const int MM = 15e4 + 5;
 int n, ans = INF, a[3][MM], dp[MM], ord[3] = {0, 1, 2};
 int main(){
 	cin.sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	//freopen("", "r", stdin); freopen("", "w", stdout);
 	sc(n);
 	for (int i = 0; i < 3; i++)
 		for (int j = 1; j <= n; j++) sc(a[i][j]);
