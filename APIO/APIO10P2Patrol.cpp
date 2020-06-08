f97c9527d70306696e5b392a9c97b5833f936394
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-06-08 14:45:32 -0400

    Update APIO10P2Patrol.cpp

--- APIO/APIO10P2Patrol.cpp
+++ APIO/APIO10P2Patrol.cpp
@@ -1,75 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define INF 0x3f3f3f3f
-#define sp ' '
-#define nl '\n'
-void sc(){}template<class T,class...A>void sc(T&t,A&...a){cin>>t,sc(a...);}
-void pr(){}template<class T,class...A>void pr(T t,A...a){cout<<t,pr(a...);}
-#define ms(x, y) memset(x, y, sizeof(x))
-#define sz(x) (int)(x.size())
-#define mp make_pair
-#define pb push_back
-#define f first
-#define s second
-const int mod = 1e9 + 7, base = 131;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-const int MM = 1e5 + 5;
-int N, K, d1, d2, far, cur, best[MM], sec[MM]; 
-pii pre[MM];
-vector<pii> adj[MM];
-void dfs(int src, int par, int d){ // diameter
-	if (d > d1) {d1 = d; far = src;}
-	for (int i = 0; i < sz(adj[src]); i++){
-		pii v = adj[src][i];
-		if (v.s == par) continue;
-		pre[v.s] = {src, i};
-		dfs(v.s, src, d + v.f);
+int read(){int s=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){s=(s<<3)+(s<<1)+ch-'0',ch=getchar();}return s*f;}
+#define re read()
+#define ms(x, y) memset(x, y, sizeof x)
+const int mod = 1e9 + 7, MM = 1e5 + 5;
+struct edge{int v, w, nxt;}edges[2*MM]; // chinese adj list makes life sm easier
+int n, m, tot, maxd, rt, ans, head[MM], best[MM], sec[MM], ch[MM][2];
+void dfs(int src, int par){
+	best[src] = sec[src] = 0, ch[src][0] = ch[src][1] = -1;
+	for (int i = head[src]; i >= 0; i = edges[i].nxt){
+		int v = edges[i].v, w = edges[i].w;
+		if (v == par) continue;
+		dfs(v, src);
+		int d = best[v] + w;
+		if (d > best[src]) sec[src] = best[src], best[src] = d, ch[src][1] = ch[src][0], ch[src][0] = i;
+		else if (d > sec[src]) sec[src] = d, ch[src][1] = i;
 	}
-}
-void change(){ // flip the edges
-	for (int x = far; x != cur; x = pre[x].f){
-		int a = pre[x].f, b = x;
-		adj[a][pre[x].s].f = -1;
-		for (int i = 0; i < sz(adj[b]); i++){
-			if (adj[b][i].s == a){
-				adj[b][i].f = -1; break;
-			}
-		}
-	}
-}
-void dp(int src, int par){ // finding diameter with negative edge
-	best[src] = sec[src] = 0;
-	int c1 = 0;
-	for (auto v : adj[src]){
-		if (v.s == par) continue;
-		c1++;
-		dp(v.s, src);
-		int d = best[v.s] + v.f;
-		if (d >= best[src]){
-			sec[src] = best[src];
-			best[src] = d;
-		}
-		else if (d > sec[src]) sec[src] = d;
-	}
-	if (!c1) return;
-	if (c1 == 1) d2 = max(d2, best[src]);
-	else if (c1 > 1) d2 = max(d2, best[src] + sec[src]);
+	if (best[src] + sec[src] > maxd) maxd = best[src] + sec[src], rt = src;
 }
 int main(){
-	cin.sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	sc(N, K);
-	for (int i = 1, a, b; i < N; i++){
-		sc(a, b);
-		adj[a].pb(mp(1, b)); adj[b].pb(mp(1, a));
+	n = re, m = re; ms(head, -1);
+	for (int i = 1; i < n; i++){
+		int a = re, b = re;
+		edges[tot] = {b, 1, head[a]}, head[a] = tot++;
+		edges[tot] = {a, 1, head[b]}, head[b] = tot++;
 	}
-	dfs(1, 1, 0); d1 = 0; cur = far;
-	dfs(far, far, 0);
-	if (K == 1) {pr(2 * (N - 1) - d1 + 1); return 0;}
-	change();
-	dp(1, 1);
-	pr(2 * N - d1 - d2);
-	return 0;
+	dfs(1, 1); ans = 2 * (n - 1) - maxd + 1; 
+	if (m == 1) return 0 * printf("%d", ans);
+	for (int k = 0; k <= 1; k++)
+		for (int i = ch[rt][k]; i != -1; i = ch[edges[i].v][0])
+			edges[i].w = edges[i ^ 1].w = -1;
+	maxd = 0; dfs(1, 1); ans -= maxd - 1;
+	printf("%d\n", ans); 
 }
-
