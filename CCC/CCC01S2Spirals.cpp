e36d4edf8410b6f9327b0223991f2b4c844973bf
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-07-01 15:21:25 -0400

    Update CCC01S2Spirals.cpp

--- CCC/CCC01S2Spirals.cpp
+++ CCC/CCC01S2Spirals.cpp
@@ -1,36 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int spiral[12][12];
-int main()
-{
-    int x, y;
-    cin >>x >>y;
-    int r = 6, c = 6, limit = 1, step = 0, turn = 0, d = 0;
-    int dir[4][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-    spiral[r][c] = x;
-    for (int i = x + 1; i <= y; i++){
-        if(step < limit){
-            r += dir[d][0]; c += dir[d][1];
-            spiral[r][c] = i;
-            step++;
-        }
-        else {
-            d = (d + 1) % 4;
-            turn++;
-            step = 0;
-            if(turn == 2){
-                limit++;
-                turn = 0;
-            }
-            i--;
-        }
-    }
-    for (int i = 0; i < 12; i++){
-        for (int j = 0; j < 12; j++){
-            if (spiral[i][j] != 0) cout <<spiral[i][j] << "\t";
-            else cout << "\t";
-        }
-        cout <<endl;
-    }
-    return 0;
+int T, st, ed, g[13][13], mx[13];
+int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+int main(){
+	scanf("%d", &T);
+	while(T--){
+		scanf("%d%d", &st, &ed); memset(g, 0, sizeof g); memset(mx, 0, sizeof mx);
+		int t = 6, b = 6, l = 6, r = 6, d = 1, td = 0, x = 6, y = 6, cur = 0; bool flag = 0;
+		g[x][y] = st;
+		for (int i = st + 1; i <= ed; i++){
+			x += dir[cur%4][0], y += dir[cur%4][1];
+			g[x][y] = i, td++;
+			if (td == d) d += flag, flag ^= 1, cur++, td = 0;
+			t = min(t, x), b = max(b, x), l = min(l, y), r = max(r, y);
+		}
+		for (int i = l; i <= r; i++)
+			for (int j = t; j <= b; j++)
+				mx[i] = max(mx[i], g[j][i]);
+		for (int i = t; i <= b; i++){
+			for (int j = l; j <= r; j++){
+				if (!g[i][j]){
+					if (mx[j] < 10) printf(" ");
+					else printf("  ");
+				}
+				else if (g[i][j] < 10 && mx[j] >= 10) printf(" %d", g[i][j]);
+				else printf("%d", g[i][j]);
+				putchar(j == r ? '\n' : ' ');
+			}
+		}
+		if (T) puts("");
+	}
 }
