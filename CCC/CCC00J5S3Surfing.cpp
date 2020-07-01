9295c38fc9bb89739720255063676fca96a9d68b
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-07-01 13:45:43 -0400

    Update and rename CCC00J5Surfing.cpp to CCC00J5S3Surfing.cpp

--- CCC/CCC00J5S3Surfing.cpp
+++ CCC/CCC00J5S3Surfing.cpp
@@ -5,23 +5,14 @@ using namespace std;
 #define ms(x, y) memset(x, y, sizeof(x))
 #define pb push_back
 #define nl '\n'
-map<string, int> dict;
 int n, t; bool vis[100000];
-string link, l, link2;
-vector<int> adj[100000];
+string link, l, link2; vector<int> adj[100000]; queue<int> q; map<string, int> dict;
 void bfs (int src){
-    ms(vis, 0);
-    queue<int> q;
-    q.push(src);
-    vis[src] = 1;
+    ms(vis, 0); q.push(src), vis[src] = 1;
     while(!q.empty()){
         int p = q.front(); q.pop();
-        for (int v : adj[p]){
-            if (!vis[v]){
-                vis[v] = 1;
-                q.push(v);
-            }
-        }
+        for (int v : adj[p])
+            if (!vis[v]) vis[v] = 1, q.push(v);
     }
 }
 int main(){
@@ -33,10 +24,7 @@ int main(){
         while(gl(l) && l != "</HTML>"){
             bool flag = 0;
             for (int i = 0; i < sz(l); i++){
-                if (l.substr(i, 6) == "A HREF"){
-                    flag = 1;
-                    i += 8;
-                }
+                if (l.substr(i, 6) == "A HREF") flag = 1, i += 8;
                 if (l[i] == '\"'){
                     flag = 0;
                     if (!dict.count(link2)) dict[link2] = ++t;
@@ -50,8 +38,7 @@ int main(){
     }
     gl(link);
     while(link != "The End"){
-        gl(link2);
-        bfs(dict[link]);
+        gl(link2); bfs(dict[link]);
         if (vis[dict[link2]]) cout << "Can surf from " << link << " to " << link2 << '.' << nl;
         else cout << "Can't surf from " << link << " to " << link2 << '.' << nl;
         gl(link);
