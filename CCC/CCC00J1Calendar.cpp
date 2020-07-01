eaf7a4df5894ea86b8708538cd13e471e7e1e49d
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-07-01 13:02:00 -0400

    Update CCC00J1Calendar.cpp

--- CCC/CCC00J1Calendar.cpp
+++ CCC/CCC00J1Calendar.cpp
@@ -1,21 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std;
-int n, days, pos = 1;
+#include <iostream>
+int a, b, cnt = 1;
 int main(){
-    cin.sync_with_stdio(0); cin.tie(0);
-    cin >> n >> days;
-    cout << "Sun Mon Tue Wed Thr Fri Sat" << endl;
-    for (int i = 0; i < n - 1; i++){
-        cout << "    ";
-        pos++;
-    }
-    for (int x = 1; x <= days; x++){
-        if (x < 10) cout << "  " << x;
-        else cout << " " << x;
-        if (pos == 7 || x == days) cout << endl;
-        else cout << ' ';
-        pos++;
-        if (pos == 8) pos = 1;
-    }
-    return 0;
+	scanf("%d%d", &a, &b);
+	puts("Sun Mon Tue Wed Thr Fri Sat");
+	for (int i = 1; i < a; i++, cnt++) printf("    ");
+	for (int i = 1; i <= b; i++, cnt++){
+		if (i < 10) printf("  %d", i);
+		else printf(" %d", i);
+		putchar(!(cnt%7) || i == b ? '\n' : ' ');
+	}
 }
