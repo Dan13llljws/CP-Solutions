af56b4a96c724b16fb8d7824e6f0d9a38d514e52
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-07-01 23:29:39 -0400

    Update CCC02J2AmeriCanadian.cpp

--- CCC/CCC02J2AmeriCanadian.cpp
+++ CCC/CCC02J2AmeriCanadian.cpp
@@ -1,19 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    string ameri;
-    cin >> ameri;
-    char vowels[6] = {'a', 'e', 'i', 'o', 'u', 'y'};
-    while (ameri != "quit!"){
+int main(){
+    string ameri, vowels = "aeiouy";
+    while (cin >> ameri, ameri != "quit!"){
         bool consonant = true;
-        for (int i = 0; i < 6; i++){
-            if (ameri[ameri.length() - 3] == vowels[i]) consonant = false;
-        }
-        if (consonant && ameri.substr(ameri.length() - 2, 2) == "or" && ameri.length() > 4)
-            ameri = ameri.substr(0, ameri.length() - 2) + "our";
+        for (int i = 0; i < 6; i++)
+            if (ameri[ameri.size() - 3] == vowels[i]) consonant = 0;
+        if (consonant && ameri.substr(ameri.size() - 2, 2) == "or" && ameri.size() > 4)
+            ameri = ameri.substr(0, ameri.size() - 2) + "our";
         cout << ameri <<endl;
-        cin >> ameri;
     }
-        return 0;
 }
