fe04cab3a507b69cfc1fe38bb4bbbcd0342e706b
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-07-05 01:00:41 -0400

    Update CCC03S5TruckingTroubles.cpp

--- CCC/CCC03S5TruckingTroubles.cpp
+++ CCC/CCC03S5TruckingTroubles.cpp
@@ -1,57 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define INF 0x3f3f3f3f
-#define sz(x) (int)(x.size())
-#define af(x) x.begin(), x.end()
-#define mp make_pair
-#define pb push_back
+typedef long long ll;
+typedef pair<int, int> pii;
+const int inf=0x3f,INF=0x3f3f3f3f; const ll LLINF=0x3f3f3f3f3f3f3f3f;
+int read(){int s=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){s=(s<<3)+(s<<1)+ch-'0',ch=getchar();}return s*f;}
+#define re read()
 #define f first
 #define s second
-#define nl '\n'
-typedef pair<int, int> pii;
-int c, r, d, dist[100001], diset[100001], ans = INF; bool vis[100001];
-vector<pair<pii, int>> edge; vector<int> dest; vector<pii> adj[100001];
-bool cmp(pair<pii, int> a, pair<pii, int> b){
-    return a.s > b.s;
-}
-int Find(int x){
-    return diset[x] == x ? x : diset[x] = Find(diset[x]);
-}
-void unite(int x, int y){
-    diset[Find(x)] = Find(y);
-}
-void dfs(int src){
-    vis[src] = 1;
-    for (int i = 0; i < sz(adj[src]); i++){
-        int u = adj[src][i].s, v = adj[src][i].f;
-        if (!vis[u]){
-            dist[u] = v;
-            dfs(u);
-        }
-    }
+const int mod = 1e9 + 7, MM = 1e4 + 5;
+struct edge{int a, b, w;};
+int n, m, q, ans = INF, dsu[MM], dist[MM];
+vector<pii> adj[MM]; vector<edge> edges;
+bool cmp(edge a, edge b){return a.w > b.w;}
+int Find(int x){return dsu[x] == x ? x : dsu[x] = Find(dsu[x]);}
+void unite(int x, int y){dsu[Find(x)] = Find(y);}
+void dfs(int src, int par){
+	for (pii v : adj[src]){
+		if (v.s == par) continue;
+		dist[v.s] = min(dist[src], v.f);
+		dfs(v.s, src);
+	}
 }
 int main(){
-    cin.sync_with_stdio(0); cin.tie(0);
-    cin >> c >> r >> d;
-    for (int i = 0, x, y, w; i < r; i++){
-        cin >> x >> y >> w;
-        edge.pb(mp(mp(x, y), w));
-    }
-    for (int i = 1; i <= c; i++) diset[i] = i;
-    sort(af(edge), cmp);
-    for (int i = 0, x; i < d; i++){
-        cin >> x;
-        dest.pb(x);
-    }
-    for (int i = 0; i < r; i++){
-        int u = edge[i].f.f, v = edge[i].f.s, w = edge[i].s;
-        if (Find(u) != Find(v)){
-            unite(u, v);
-            adj[u].pb(mp(w, v)); adj[v].pb(mp(w, u));
-        }
-    }
-    dfs(1);
-    for (int i = 0; i < d; i++) ans = min(ans, dist[dest[i]]);
-    cout << ans;
-    return 0;
+	n = re, m = re, q = re, dist[1] = INF;
+	for (int i = 0; i < m; i++) edges.push_back({re, re, re});
+	sort(edges.begin(), edges.end(), cmp);
+	for (int i = 1; i <= n; i++) dsu[i] = i;
+	for (edge v : edges)
+		if (Find(v.a) != Find(v.b)){
+			unite(v.a, v.b);
+			adj[v.a].push_back({v.w, v.b}), adj[v.b].push_back({v.w, v.a});
+		}
+	dfs(1, 1);
+	for (int i = 0; i < q; i++) ans = min(ans, dist[re]);
+	printf("%d", ans);
 }
