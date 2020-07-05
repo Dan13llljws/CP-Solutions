7aa0a43932bafa41f39cdf98bc19b0454b66f260
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-07-05 00:15:32 -0400

    Update and rename CCC03S2Poetry.cpp to CCC03J4S2Poetry.cpp

--- CCC/CCC03J4S2Poetry.cpp
+++ CCC/CCC03J4S2Poetry.cpp
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+string vowels = "aeiou";
 string lastSlb(string str){
-    string vowels = "aeiou";
-    for (int i = str.length() - 1; i >= 0; i--){
+    for (int i = str.length() - 1; i >= 0; i--)
         for (int j = 0; j < 5; j++){
             if (str[i] < 97 && str[i] != 32) str[i] += 32;
             if (str[i] == vowels[j]) return str.substr(i,str.length() - i + 1);
             else if (str[i] == ' ') return str.substr(i,str.length() - i + 1);
             else if (i == 0) return str;
         }
-    }
 }
-int main()
-{
-    int nVerse;
-    cin >>nVerse;
-    cin.ignore();
+int main(){
+	cin.sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int nVerse; cin >>nVerse; cin.ignore();
     for (int i = 0; i < nVerse; i++){
-        string lines[4];
-        string lastS[4];
-        for (int i = 0; i < 4; i++){
-            getline (cin, lines[i]);
-        }
+        string lines[4], lastS[4];
+        for (int j = 0; j < 4; j++) getline (cin, lines[j]);
         lastS [0] = lastSlb(lines[0]);
         lastS [1] = lastSlb(lines[1]);
         lastS [2] = lastSlb(lines[2]);
@@ -32,5 +26,4 @@ int main()
         else if (lastS[0] == lastS[3] && lastS[1] == lastS[2]) cout << "shell"<<endl;
         else cout << "free"<<endl;
     }
-    return 0;
 }
