7e4b315f686c5f40a11f1976ced72c9e47badac3
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-07-21 22:53:33 -0400

    Update COCI14Contest6#5Neo.cpp

--- COCI/COCI14Contest6#5Neo.cpp
+++ COCI/COCI14Contest6#5Neo.cpp
@@ -3,18 +3,15 @@ using namespace std;
 int read(){int s=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){s=(s<<3)+(s<<1)+ch-'0',ch=getchar();}return s*f;}
 #define re read()
 const int mod = 1e9 + 7, MM = 1e3 + 5;
-int r, c, maxa, t, st[MM], g[MM][MM], h[MM]; bool ok[MM][MM];
+int r, c, maxa, t, st[MM], g[MM][MM], h[MM];
 int main(){
 	r = re, c = re;
 	for (int i = 0; i < r; i++)
 		for (int j = 0; j < c; j++)
 			g[i][j] = re;
-	for (int i = 1; i < r; i++)
-		for (int j = 1; j < c; j++)
-			ok[i][j] = (g[i][j] + g[i - 1][j - 1] <= g[i - 1][j] + g[i][j - 1]);
 	for (int i = 1; i < r; i++){
 		for (int j = 1; j < c; j++)
-			if (ok[i][j]) h[j]++;
+			if (g[i][j] + g[i - 1][j - 1] <= g[i - 1][j] + g[i][j - 1]) h[j]++;
 			else h[j] = 0;
 		t = 0;
 		for (int j = 1; j <= c; j++){
