961188dfb28d9facec2cace51ececc6a540fd402
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-10-02 11:11:43 -0400

    Update COCI15Contest2#5Vudu.cpp

--- COCI/COCI15Contest2#5Vudu.cpp
+++ COCI/COCI15Contest2#5Vudu.cpp
@@ -4,7 +4,7 @@ typedef long long ll;
 int read(){int s=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){s=(s<<3)+(s<<1)+ch-'0',ch=getchar();}return s*f;}
 #define re read()
 const int mod = 1e9 + 7, MM = 1e6 + 5;
-int n, p, a[MM], b[MM], bit[MM]; ll ans, psa[MM]; vector<ll> comp;
+int n, p, a[MM], bit[MM]; ll ans, psa[MM]; vector<ll> comp;
 void update(int x){for(; x <= (int)comp.size(); x += x & -x) bit[x]++;}
 int get(int x){
 	int ret = 0;
@@ -19,8 +19,8 @@ int main(){
 	sort(comp.begin(), comp.end()); comp.resize(unique(comp.begin(), comp.end()) - comp.begin());
 	update(lower_bound(comp.begin(), comp.end(), 0) - comp.begin() + 1);
 	for (int i = 1; i <= n; i++){
-		b[i] = lower_bound(comp.begin(), comp.end(), psa[i]) - comp.begin() + 1;
-		ans += get(b[i]), update(b[i]);
+		int tmp = lower_bound(comp.begin(), comp.end(), psa[i]) - comp.begin() + 1;
+		ans += get(tmp), update(tmp);
 	}
 	printf("%lld\n", ans);
 }
