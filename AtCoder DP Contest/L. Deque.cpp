2d9d9744e6e2079c1971ba5abc990fe63a6a1ff8
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-10-17 15:24:13 -0400

    Update L. Deque.cpp

--- AtCoder DP Contest/L. Deque.cpp
+++ AtCoder DP Contest/L. Deque.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int read(){int s=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){s=(s<<3)+(s<<1)+ch-'0',ch=getchar();}return s*f;}
-typedef long long ll;
-int n; ll dp[3005][3005];
+int n; long long dp[3005][3005];
 int main(){
 	n = read();
 	for (int i = 1; i <= n; i++) dp[i][i] = read();
