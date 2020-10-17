6b795f5b3c1a30077a2a57c2f6673d1eddf7df09
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-10-17 15:22:40 -0400

    Update W. Intervals.cpp

--- AtCoder DP Contest/W. Intervals.cpp
+++ AtCoder DP Contest/W. Intervals.cpp
@@ -1,22 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 int read(){int s=0,f=1;char ch=getchar();while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}while(ch>='0'&&ch<='9'){s=(s<<3)+(s<<1)+ch-'0',ch=getchar();}return s*f;}
-#define re read()
 #define lc i << 1
 #define rc i << 1 | 1
 const int mod = 1e9 + 7, MM = 2e5 + 5;
 struct interval{int l, r, v;} a[MM];
-struct node{ll v, lz;} seg[MM<<2];
-int n, m; ll sum, dp;
+struct node{long long v, lz;} seg[MM<<2];
+int n, m; long long sum, dp;
 void push_down(int i){
-	if (seg[i].lz){
-		seg[lc].lz += seg[i].lz, seg[rc].lz += seg[i].lz;
-		seg[lc].v += seg[i].lz, seg[rc].v += seg[i].lz;
-		seg[i].lz = 0;
-	}
+	if (!seg[i].lz) return;
+	seg[lc].lz += seg[i].lz, seg[rc].lz += seg[i].lz;
+	seg[lc].v += seg[i].lz, seg[rc].v += seg[i].lz;
+	seg[i].lz = 0;
 }
-void update(int i, int l, int r, ll v, int l1 = 0, int r1 = n + 1){
+void update(int i, int l, int r, long long v, int l1 = 0, int r1 = n + 1){
 	if (l == l1 && r == r1){seg[i].v += v, seg[i].lz += v; return;}
 	push_down(i);
 	int mid = (l1 + r1) >> 1;
@@ -25,7 +22,7 @@ void update(int i, int l, int r, ll v, int l1 = 0, int r1 = n + 1){
 	else update(lc, l, mid, v, l1, mid), update(rc, mid + 1, r, v, mid + 1, r1);
 	seg[i].v = min(seg[lc].v, seg[rc].v);
 }
-ll get(int i, int l, int r, int l1 = 0, int r1 = n + 1){
+long long get(int i, int l, int r, int l1 = 0, int r1 = n + 1){
 	if (l == l1 && r == r1) return seg[i].v;
 	push_down(i);
 	int mid = (l1 + r1) >> 1;
@@ -34,8 +31,8 @@ ll get(int i, int l, int r, int l1 = 0, int r1 = n + 1){
 	return min(get(lc, l, mid, l1, mid), get(rc, mid + 1, r, mid + 1, r1));
 }
 int main(){
-	n = re, m = re;
-	for (int i = 0; i < m; i++) a[i] =  {re, re, re}, sum += a[i].v;
+	n = read(), m = read();
+	for (int i = 0; i < m; i++) a[i] =  {read(), read(), read()}, sum += a[i].v;
 	sort(a, a + m, [](const interval& a1, const interval& a2){return a1.r < a2.r;});
 	int ptr = 0;
 	for (int i = 1; i <= n + 1; i++){
