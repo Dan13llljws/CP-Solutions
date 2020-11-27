98b98678e2e13496668c331c7ab72cc8a64f1f8e
Dan13llljws <59179986+Dan13llljws@users.noreply.github.com>
2020-11-27 09:35:51 -0500

    Update COCI19Contest5#5Zapina.cpp

--- COCI/COCI19Contest5#5Zapina.cpp
+++ COCI/COCI19Contest5#5Zapina.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 typedef long long ll;
 const int mod = 1e9 + 7;
-int n; ll dp[355][355], fac[355], inv[355];
+int n; ll c[355][355], dp[355][355];
 ll fpow(ll a, ll b){
 	ll ret = 1;
 	while(b){
@@ -11,19 +11,20 @@ ll fpow(ll a, ll b){
 	}
 	return ret;
 }
-ll choose(int a, int b){return fac[a] * inv[b] % mod * inv[a - b] % mod;}
 int main(){
-	scanf("%d", &n);fac[0] = inv[0] = 1;
-	for (int i = 1; i <= n; i++) fac[i] = fac[i - 1] * i % mod;
-	inv[n] = fpow(fac[n], mod - 2);
-	for (int i = n - 1; i; i--) inv[i] = inv[i + 1] * (i + 1) % mod;
+	scanf("%d", &n);
+	for (int i = 0; i <= n; i++){
+		c[i][0] = c[i][i] = 1;
+		for (int j = 1; j < i; j++)
+			c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % mod;
+	}
 	dp[1][0] = 1;
 	for (int i = 2; i <= n; i++) dp[1][i] = 1;
 	for (int i = 1; i < n; i++)
 		for (int j = 0; j <= n; j++)
 			for (int k = 0; k <= n - j; k++)
 				if (k != i + 1)
-					dp[i + 1][j + k] = (dp[i + 1][j + k] + dp[i][j] * choose(j + k, k) % mod) % mod;
+					dp[i + 1][j + k] = (dp[i + 1][j + k] + dp[i][j] * c[j + k][k] % mod) % mod;
 	printf("%lld\n", (fpow(n, n) - dp[n][n] + mod) % mod);
 }
 
