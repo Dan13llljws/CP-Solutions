2f57f4c6cc5b832bdd1e52566a0cecf5ab40ad8e
Dan13llljws <dan13jws@gmail.com>
2023-05-17 23:41:52 -0400

    new solution

--- CSES/2162-josephus-problem-i.cpp
+++ CSES/2162-josephus-problem-i.cpp
@@ -4,13 +4,11 @@ int main(){
     cin.sync_with_stdio(0); cin.tie(0);
     int n;
     cin >> n;
-    vector<int> nxt(n), done(n);
-    for (int i = 0; i < n - 1; i++) nxt[i] = i + 1;
-    nxt[n - 1] = 0;
+    vector<int> nxt(n);
+    for (int i = 0; i < n; i++) nxt[i] = (i + 1) % n;
     for (int i = 0, j = 0; i < n; i++){
         cout << nxt[j] + 1 << ' ';
         nxt[j] = nxt[nxt[j]];
-        done[j] = 1;
         j = nxt[j];
     }
     cout << '\n';
