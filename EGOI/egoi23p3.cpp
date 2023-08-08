2c992f7d78881ea156b2081efb42b5b0bf0ddb29
Dan13llljws <dan13jws@gmail.com>
2023-08-08 01:53:42 -0400

    new solution

--- EGOI/egoi23p3.cpp
+++ EGOI/egoi23p3.cpp
@@ -6,40 +6,30 @@ int main(){
 	int x, y;
 	auto answer = [&](int x, int y) -> void {
 		cout << "! " << x << ' ' << y << endl;
-		exit(0);
+	};
+	auto ask = [&](string &s) -> void {
+		cout << "? " << s << endl;
+		cin >> x >> y;
 	};
 	int m = w / 2;
-	cout << "? ";
-	for (int i = 0; i < m; i++) cout << ">";
+	string s(m, '>');
 	for (int i = 0; i < h; i++){
-		for (int j = m; j < w - 1; j++) cout << ">";
-		if (i != h - 1){
-			for (int j = m; j < w - 1; j++) cout << "<";
-			cout << "v";
-		}
-	} 
-	cout << endl; cin >> x >> y;
-	if (x != h - 1) {
-		answer(x + 1, m);
-	} else if (y == w - 1){
-		cout << "? ";
-		for (int i = 0; i < m; i++) cout << ">";
+		s += string(w - m - 1, '>');
+		if (i != h - 1) s += string(w - m - 1, '<') + "v";
+	} ask(s);
+	if (x != h - 1) answer(x + 1, m); 
+	else if (y == w - 1){
+		s = string(m, '>');
 		for (int i = 0; i < h; i++){
-			for (int j = 1; j < m; j++) cout << "<";
-			if (i != h - 1){
-				for (int j = 1; j < m; j++) cout << ">";
-				cout << "v";
-			}
-		} cout << "<";
-		for (int i = 0; i < h - 1; i++) cout << "^";
-		cout << endl; cin >> x >> y;
+			s += string(m - 1, '<');
+			if (i != h - 1) s += string(m - 1, '>') + "v";
+		} s += "<" + string(h - 1, '^');
+		ask(s);
 		if (!x) answer(h - 1, y - 1);
 		else answer(x - 1, y);
 	} else {
-		cout << "? ";
-		for (int i = 0; i < y + 1; i++) cout << ">";
-		for (int i = 0; i < h; i++) cout << "v";
-		cout << endl; cin >> x >> y;
+		s = string(y + 1, '>') + string(h, 'v');
+		ask(s);
 		if (x == h - 1) answer(0, y + 1);
 		else answer(x + 1, y);
 	}
